c0d32770cdbb528ece2db09b7a8a5dc45a580f60
LAIN <jrafael.pfz@gmail.com>
2022-04-23 14:42:54 -0400

    [feature] - create add to cart function

--- src/App.js
+++ src/App.js
@@ -11,14 +11,43 @@ class App extends Component {
       { name: 'Serrucho', price: 2500, img: '/products/serrucho.jpg' },
       { name: 'Alicate', price: 3000, img: '/products/alicate.jpg' },
     ],
+    cart: [
+      // { name: 'Alicate', price: 3000, img: '/products/alicate.jpg', quantity: 1 }
+    ],
   };
+
+  addToCart = (product) => {
+    const { cart } = this.state;
+    if (cart.find((x) => x.name === product.name)) {
+      const newCart = cart.map((x) =>
+        x.name === product.name
+          ? {
+              ...x,
+              quantity: x.quantity + 1,
+            }
+          : x
+      );
+      return this.setState({ cart: newCart });
+    }
+    return this.setState({
+      cart: this.state.cart.concat({
+        ...product,
+        quantity: 1,
+      }),
+    });
+  };
+
+  componentDidUpdate() {
+    console.log(this.state.cart);
+  }
+
   render() {
     return (
       <div>
         <Navbar />
         <Layout>
           <Title />
-          <Products addToCart={() => null} products={this.state.products} />
+          <Products addToCart={this.addToCart} products={this.state.products} />
         </Layout>
       </div>
     );
