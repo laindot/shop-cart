f6886e56bc1891ddb1af541c5618975da92044b3
LAIN <jrafael.pfz@gmail.com>
2022-04-23 12:19:34 -0400

    [feature] - create shop cart app products & navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import Products from './components/products';
+import Layout from './components/layout';
+import Title from './components/title';
+import Navbar from './components/navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    products: [
+      { name: 'Martillo', price: 1500, img: '/products/martillo.jpg' },
+      { name: 'Serrucho', price: 2500, img: '/products/serrucho.jpg' },
+      { name: 'Alicate', price: 3000, img: '/products/alicate.jpg' },
+    ],
+  };
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Layout>
+          <Title />
+          <Products addToCart={() => null} products={this.state.products} />
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/bubbleAlert.js
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+
+const styles = {
+  bubbleAlert: {
+    backgroundColor: '#E9725A',
+    color: '#fff',
+    borderRadius: '15px',
+    padding: '2px 10px',
+    fontSize: '0.9rem',
+    width: '20px',
+  },
+};
+
+class BubbleAlert extends Component {
+  render() {
+    return <span style={styles.bubbleAlert}>5</span>;
+  }
+}
+
+export default BubbleAlert;
